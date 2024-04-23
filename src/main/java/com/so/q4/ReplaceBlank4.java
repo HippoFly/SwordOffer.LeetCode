d954c5dbc60a5f5ea0f92d1c47465706dc1a7f6e
FlyHippo <whoarewe4u@protonmail.com>
2024-04-23 14:30:23 +0800

    q4字符串替换

---
+++ src/main/java/com/so/q4/ReplaceBlank4.java
@@ -0,0 +1,90 @@
+package com.so.q4;
+
+import java.util.Arrays;
+
+/**
+ * 第4题
+ * 将一个字符串中的空格替换成"%20"
+ *
+ */
+public class ReplaceBlank4 {
+
+    /**
+     * 解法一：使用StringBuffer
+     *
+     * @param input
+     * @return
+     */
+    public static String replaceBlank1(String input) {
+        if (input == null) {
+            return null;
+        }
+        StringBuffer outputBuffer = new StringBuffer();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                outputBuffer.append("%20");
+            } else {
+                outputBuffer.append(input.charAt(i));
+            }
+        }
+
+        return outputBuffer.toString();
+    }
+
+    /**
+     * 解法二：使用StringBuilder
+     *
+     * @param input
+     * @return
+     */
+    public static String replaceBlank2(String input) {
+        if (input == null) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (" ".equals(String.valueOf(input.charAt(i)))) {
+                sb.append("%20");
+            } else {
+                sb.append(input.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 解法三：从后往前复制
+     *
+     * @param input
+     * @return
+     */
+    public static String replaceBlank3(String input) {
+        if (input == null) {
+            return null;
+        }
+        int blankCount = 0;
+        int length = input.length();
+        int newLength = 0;
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) == ' ') {
+                blankCount++;
+            }
+        }
+        // 替换后的字符串长度
+        newLength = length + 2 * blankCount;
+        char[] newChars = new char[newLength];
+        int index = newLength - 1;
+        for (int i = length - 1; i >= 0; i--) {
+            if (input.charAt(i) == ' ') {
+                newChars[index--] = '0';
+                newChars[index--] = '2';
+                newChars[index--] = '%';
+            } else {
+                newChars[index--] = input.charAt(i);
+            }
+        }
+        return Arrays.toString(newChars);
+    }
+}
---
+++ src/test/java/com/so/q4/Test4.java
@@ -0,0 +1,19 @@
+package com.so.q4;
+
+import com.so.q4.ReplaceBlank4;
+import org.junit.Test;
+
+/**
+ * 第4题
+ * 将一个字符串中的空格替换成"%20"
+ *
+ */
+public class Test4 {
+    @Test
+    public void test4() {
+        String beforeStr = " ab g gt r ";
+        System.out.println("解法一：使用StringBuffer， 替换前：" + beforeStr + " 替换后：" + ReplaceBlank4.replaceBlank1(beforeStr));
+        System.out.println("解法二：使用StringBuilder，替换前：" + beforeStr + " 替换后：" + ReplaceBlank4.replaceBlank2(beforeStr));
+        System.out.println("解法三：从后往前复制，     替换前：" + beforeStr + " 替换后：" + ReplaceBlank4.replaceBlank3(beforeStr));
+    }
+}

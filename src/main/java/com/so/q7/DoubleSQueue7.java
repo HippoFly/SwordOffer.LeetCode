f82e0c32e36f6b03db65f12f55d89604779f74e0
FlyHippo <whoarewe4u@protonmail.com>
2024-04-25 17:47:46 +0800

    q7双stack成queue

---
+++ src/main/java/com/so/q7/DoubleSQueue7.java
@@ -0,0 +1,44 @@
+package com.so.q7;
+
+import java.util.Stack;
+
+/**
+ * 第7题
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ * 入只入1
+ * 出2，双空报错，2空拿1入
+ */
+public class DoubleSQueue7 {
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    /**
+     * 入栈
+     * 时间复杂度：O(1)，空间复杂度：O(n)
+     *
+     * @param node
+     */
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    /**
+     * 出栈
+     * 时间（摊还）复杂度：O(1)，空间复杂度：O(1)
+     *
+     * @return
+     * @throws Exception
+     */
+    public int pop() throws Exception {
+        if (stack1.isEmpty() && stack2.isEmpty()) {
+            throw new Exception("栈为空！");
+        }
+
+        if (stack2.isEmpty()) {
+            while(!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+}
---
+++ src/test/java/com/so/q7/Test7.java
@@ -0,0 +1,20 @@
+package com.so.q7;
+
+import org.junit.Test;
+
+/**
+ * 第7题
+ * Stack -> Queue(FIFO)
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ *
+ */
+public class Test7 {
+    @Test
+    public void test7() throws Exception {
+        DoubleSQueue7 doubleSQueue7 = new DoubleSQueue7();
+        doubleSQueue7.push(2);
+        doubleSQueue7.push(5);
+        doubleSQueue7.push(8);
+        System.out.println("出队列：" + doubleSQueue7.pop());
+    }
+}

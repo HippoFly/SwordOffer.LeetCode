ca64427b583f8ff7a3454615ba81f6f46049dc37
FlyHippo <whoarewe4u@protonmail.com>
2024-04-28 17:27:51 +0800

    HJ10.字符个数统计

---
+++ src/main/java/com/so/lc/nonrepeatnum/NonRepeatNum.java
@@ -0,0 +1,23 @@
+package com.so.lc.nonrepeatnum;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NonRepeatNum {
+    public static void main(String[] args) {
+
+    }
+
+    public static void s(){
+        Scanner in = new Scanner(System.in);
+        // 注意 hasNext 和 hasNextLine 的区别
+        String inStr= in.nextLine() ;
+        int length=inStr.length();
+        Set<Character> set=new HashSet();
+        for(int i=0;i<length;i++){
+            set.add(inStr.charAt(i));
+        }
+        System.out.println(set.size());
+    }
+}

7bd7a6e594ce7da9be6e84f16b1f249a6cd3ea38
FlyHippo <whoarewe4u@protonmail.com>
2024-05-04 11:18:35 +0800

    docs(lc35): 详细说明二分查找退出循环的原理

--- src/main/java/com/so/lc/Array/Q35_SearchInsertPosition.java
+++ src/main/java/com/so/lc/Array/Q35_SearchInsertPosition.java
@@ -20,7 +20,15 @@ public class Q35_SearchInsertPosition {
         if (nums[len - 1] < target) {
             return len;
         }
+        // [1,3,5,6](5)为案例 left因num[1]<5而变2，然后mid=2,num[mid]得5没法小于target，
+        // 让right=mid, 这退出循环
+        // 最终目的是让left步进从而mid处等于target，同时左无法满足mid小于target
+        // right直接压过来等于mid
 
+        // [1,3,6,7](5)为案例 left因num[1]<5而变2，然后mid=2,num[mid]得6没法小于target=5，
+        // 让right=mid, 这退出循环
+        // 最终目的是让left步进从而mid处没法小于target，同时左无法满足mid小于target
+        // right直接压过来等于mid， 此时由于‘/’符号去尾号的效应刚好会等于目标插处
         while (left<right){
             int mid = (left+right)/2;
             if(nums[mid]<target){
@@ -31,5 +39,10 @@ public class Q35_SearchInsertPosition {
             }
         }
         return right;
+    }
+
+    public static void main(String[] args) {
+        Q35_SearchInsertPosition insertPosition = new Q35_SearchInsertPosition();
+        System.out.println(insertPosition.searchInsert(new int[]{1, 3,  6,7}, 5));
     }
 }

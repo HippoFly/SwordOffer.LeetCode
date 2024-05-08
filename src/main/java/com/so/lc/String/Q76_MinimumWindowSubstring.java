070c59d9076365bfa6d688371f99f371518bd4d5
FlyHippo <whoarewe4u@protonmail.com>
2024-05-08 18:24:43 +0800

    docs(lc76): 阐述更加明确

--- src/main/java/com/so/lc/String/Q76_MinimumWindowSubstring.java
+++ src/main/java/com/so/lc/String/Q76_MinimumWindowSubstring.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 /**
  * 描述
- *
+ * 覆盖target字符串的最小子串
  * @author FlyHippo
  * @version 1.0
  * @createDate 2024/5/5 10:44
@@ -13,6 +13,14 @@ import java.util.Map;
 
 public class Q76_MinimumWindowSubstring {
     // 覆盖字串返回且唯一，请返回
+    // 1 挪右
+    // 2 计算后果
+    // 3 后果满足进循环
+
+    // 4 刚进满足循环，结果记录（处理）
+
+    // 5 缩左
+    // 6 计算后果
     public String minWindow(String s, String t) {
         Map<Character, Integer> targetFreq = new HashMap<>();
         Map<Character, Integer> windowFreq = new HashMap<>();
@@ -34,26 +42,27 @@ public class Q76_MinimumWindowSubstring {
         // 移动右指针，扩展窗口
         while (right < s.length()) {
 
-            // 右边界填字符
+            // （1）右边界：窗口右，先放字符再→挪
             char c = s.charAt(right);
             // 更新窗口中字符的出现频率
             windowFreq.put(c, windowFreq.getOrDefault(c, 0) + 1);
             right++;
 
-            // 单个字符频率匹配
-            // 窗口中当前字符的出现频率等于目标字符串中该字符的出现频率，则匹配数加1(不是整个词组匹配)
+            // （2）判断：某单个字符是否有且完全频率相等，相等则匹配数加1
             if (targetFreq.containsKey(c) && windowFreq.get(c).intValue() == targetFreq.get(c).intValue()) {
                 matchCount++;
             }
 
+            // （3）进一步判断（进循环尝试缩圈）： 是否计数器已经等于长短
             // 窗口中包含了目标字符串，则尝试缩小窗口
             while (matchCount == targetFreq.size()) {
-                // 更新最小 左边界 有边界
+                // 符合就判断是否满足最小窗口，记录下最小坐标
                 if (right - left < minRight-minLeft) {
                     minRight = right  ;
                     minLeft = left;
                 }
 
+                // （4）（符合）挪左
                 // 左边界右移，移除左字符
                 char leftCharInWindow = s.charAt(left);
                 left++;

2bd69fdf6332c4ed9fa69dec713f433b668dc327
FlyHippo <whoarewe4u@protonmail.com>
2024-05-11 18:46:37 +0800

    feat （Lc206）: 翻转链表

---
+++ src/main/java/com/so/lc/LinkedList/Q206_ReverseLinkedList.java
@@ -0,0 +1,34 @@
+package com.so.lc.LinkedList;
+
+import com.so.common.ListNode;
+
+/**
+ * 描述
+ *
+ * @author FlyHippo
+ * @version 1.0
+ * @createDate 2024/5/11 18:43
+ **/
+
+public class Q206_ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        // 前
+        ListNode prev = null;
+        // 现在
+        ListNode curr = head;
+        // 后
+        ListNode next = null;
+        while (curr != null) {
+            //先指针指向下一节点，记录下载，防止断连
+            next = curr.next;
+
+            // 指针反转前
+            curr.next = prev;
+
+            // 整体指针后挪
+            prev=curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

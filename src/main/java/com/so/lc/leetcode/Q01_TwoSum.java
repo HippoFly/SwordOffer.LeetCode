ac9920adb8ff9791bee7b742e59d38e9d4a1b33b
hippocanfly <hippomayfly@gamil.com>
2025-04-02 17:49:10 +0800

    feat(Q01_TwoSum):优化两数之和问题的解决方案
    - 添加了详细的类和方法注释，解释了问题描述和示例
    - 将 twoSum 方法改为静态方法，便于调用
    - 增加了 main 方法，用于测试解决方案
    -优化了代码结构，提高了可读性和可维护性

--- src/main/java/com/so/lc/leetcode/Q01_TwoSum.java
+++ src/main/java/com/so/lc/leetcode/Q01_TwoSum.java
@@ -1,9 +1,26 @@
 package com.so.lc.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
- * 描述
+ * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 2 <= nums.length <= 104
+ * -109 <= nums[i] <= 109
+ * -109 <= target <= 109
+ * 只会存在一个有效答案
+ * <p>
+ * 示例 1：
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * 示例 2：
+ * 输入：nums = [3,2,4], target = 6
+ * 输出：[1,2]
  *
  * @author FlyHippo
  * @version 1.0
@@ -11,7 +28,16 @@ import java.util.HashMap;
  **/
 
 public class Q01_TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+
+    /**
+     * 寻找两个数，使它们的和等于目标值
+     * 此方法使用哈希表来优化查找过程，避免了不必要的重复搜索
+     *
+     * @param nums 输入的整数数组
+     * @param target 目标值，我们需要找到两个数的和等于这个值
+     * @return 返回一个包含这两个数索引的数组如果找不到这样的两个数，则返回null
+     */
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             Integer index = map.get(target - nums[i]);
@@ -27,5 +53,6 @@ public class Q01_TwoSum {
 
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(twoSum(new int[]{9,2, 7, 11, 15}, 9)));
     }
 }

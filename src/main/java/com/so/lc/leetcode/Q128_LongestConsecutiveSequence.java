7674dab84b8cf6c67b332f9b9e6c4c80ad9e8301
hippocanfly <hippomayfly@gamil.com>
2025-04-03 15:27:07 +0800

    refactor(Q128):优化最长连续序列代码
    - 添加问题描述和示例
    -增加代码注释
    - 优化 main 方法，添加输出语句
    - 更新类名和方法名以符合规范
    - 移除冗余代码和不必要的空行

--- src/main/java/com/so/lc/leetcode/Q128_LongestConsecutiveSequence.java
+++ src/main/java/com/so/lc/leetcode/Q128_LongestConsecutiveSequence.java
@@ -4,11 +4,27 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 描述
+ * 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+ * <p>
+ * 请你设计并实现时间复杂度为 O(n) 的算法解决此问题
  *
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [100,4,200,1,3,2]
+ * 输出：4
+ * 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+ *
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+ * 输出：9
  * @author FlyHippo
  * @version 1.0
  * @createDate 2024/5/1 15:51
+ * @tag 哈希
+ * @link <a href="https://leetcode.cn/problems/longest-consecutive-sequence/description/">最长连续</a>
  **/
 
 public class Q128_LongestConsecutiveSequence {
@@ -27,8 +43,8 @@ public class Q128_LongestConsecutiveSequence {
      * 元素1是开头，因为没有0，且以1开头的序列长度为4，因为依次累加，2，3，4都存在。
      * 元素3不是开头，因为2存在，过，
      * 元素2不是开头，因为1存在，过。
-     * @param nums
-     * @return
+     * @param nums 输入的整数数组
+     * @return 返回最长连续序列的长度
      */
     public static int longestConsecutive(int[] nums) {
         Set<Integer> num_set = new HashSet<Integer>();
@@ -58,7 +74,8 @@ public class Q128_LongestConsecutiveSequence {
     }
 
     public static void main(String[] args) {
-        longestConsecutive(new int[]{1, 3, 2, 55, 2, 11, 23, 321, 24});
+        System.out.println(longestConsecutive(new int[]{1, 3, 2, 55, 2, 11, 23, 321, 24}));
+
     }
 
 }

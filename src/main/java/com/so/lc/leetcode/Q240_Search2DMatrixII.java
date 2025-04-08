709a8026396a3d68be8fb172756911363b348b08
hippocanfly <hippomayfly@gamil.com>
2025-04-08 15:59:18 +0800

    refactor(leetcode): 重构 Q240 搜索二维矩阵 II
    - 添加了类和方法的详细注释，解释了矩阵的特性、方法的工作原理和参数的含义
    - 优化了代码结构，使其更加清晰易读
    - 保留了原有的功能和逻辑

--- src/main/java/com/so/lc/leetcode/Q240_Search2DMatrixII.java
+++ src/main/java/com/so/lc/leetcode/Q240_Search2DMatrixII.java
@@ -1,7 +1,11 @@
 package com.so.lc.leetcode;
 
 /**
- * 描述
+ * 240. 搜索二维矩阵 II
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
  *
  * @author FlyHippo
  * @version 1.0
@@ -9,22 +13,35 @@ package com.so.lc.leetcode;
  **/
 
 public class Q240_Search2DMatrixII {
+    /**
+     * 在二维矩阵中搜索目标值
+     * 该方法利用矩阵每一行都按照从左到右递增的顺序排序，以及每一列都按照从上到下递增的顺序排序的特性
+     * 通过从矩阵右上角开始比较，逐步缩小搜索范围，以高效地找到目标值
+     *
+     * @param matrix 二维矩阵，其中每个数组代表一行，且每行和每列都是升序的
+     * @param target 目标值，要在矩阵中找到的值
+     * @return 如果找到目标值，则返回true；否则返回false
+     */
     public boolean searchMatrix(int[][] matrix, int target) {
+        // 获取矩阵的行数
         int length = matrix.length ;
+        // 初始化搜索的起始位置为矩阵的右上角
         int x = 0, y = matrix[0].length - 1;
+        // 当前行数小于矩阵总行数且当前列数不小于0时，继续搜索
         while (x < length && y >= 0) {
+            // 如果当前元素等于目标值，返回true
             if (matrix[x][y] == target) {
                 return true;
             }
-            // 一行的最顶端（最大） 大于它，需要向底部挪动
+            // 一行的最右端（最大） 大于它，需要向左挪动
             if (matrix[x][y] > target) {
                 --y;
-
-                // 一行的最顶端（最大） 小于于它，需要向更大一行挪动
+                // 否则代表这一行都小于它，下一行
             } else {
                 ++x;
             }
         }
+        // 如果没有找到目标值，返回false
         return false;
     }
 }

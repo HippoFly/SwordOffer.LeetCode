3ec5ea7055bb8386d4a18b1426b1c5fbee1c2e47
hippocanfly <hippomayfly@gamil.com>
2025-04-09 14:50:11 +0800

    refactor(Q234):优化回文链表的判断逻辑
    - 移除了不必要的快慢指针和链表反转操作- 采用遍历链表并将节点值存入列表的方法
    - 使用双指针从列表两端向中间比较，判断是否为回文
    - 代码结构更简洁，逻辑更清晰

--- src/main/java/com/so/lc/leetcode/Q234_PalindromeLinkedList.java
+++ src/main/java/com/so/lc/leetcode/Q234_PalindromeLinkedList.java
@@ -6,67 +6,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 描述
+ * 234. 回文链表
+ * <p>
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
  *
  * @author FlyHippo
  * @version 1.0
  * @createDate 2024/5/11 18:47
+ * @tag 链表，快慢指针
  **/
 
 public class Q234_PalindromeLinkedList {
 
-    public boolean isPalindrome(ListNode head) {
-        ListNode fast = head, slow = head;
-        //通过快慢指针找到中点
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        //如果fast不为空，说明链表的长度是奇数个
-        if (fast != null) {
-        // 对于奇数，中点不需要翻转
-            slow = slow.next;
-        }
-        //反转后半部分链表
-        slow = reverse(slow);
 
-        // 这里fast跟快没关系，只是复用指针，从头开始，而slow从尾巴开始
-        fast = head;
-        while (slow != null) {
-            //然后比较，判断节点值是否相等
-            if (fast.val != slow.val)
-                return false;
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return true;
-    }
-    //反转链表
-    public ListNode reverse(ListNode head) {
-        ListNode prev = null;
-        while (head != null) {
-            ListNode next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
-    public boolean isPalindrome2(ListNode head) {
+    /**
+     * 遍历+双指针
+     * @param head
+     * @return
+     */
+    public boolean isPalindrome(ListNode head) {
+        // 创建一个列表来存储链表的节点值
         List<Integer> integerList = new ArrayList<>();
+
+        // 遍历链表，将节点值存入列表
         while (head != null) {
             integerList.add(head.val);
             head = head.next;
         }
-        // 4/2=2  <[2]  [0][1][2][3]
-        // 5/2=2|  <[2]   [0][1][2][3][4]
-        boolean flag = true;
-        for (int i = 0; i < integerList.size()/2; i++) {
-            if(integerList.get(i)!=integerList.get(integerList.size()-1-i)){
-                flag=false;
-                break;
+
+        // 使用双指针从列表的两端向中间比较
+        int left = 0;
+        int right = integerList.size() - 1;
+        while (left < right) {
+            if (!integerList.get(left).equals(integerList.get(right))) {
+                return false;
             }
+            left++;
+            right--;
         }
-        return flag;
+
+        // 如果所有对应位置的元素都相等，则返回 true
+        return true;
     }
 }

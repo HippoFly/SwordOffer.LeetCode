8e47f2dd81067564a730004c1bd4bba336fcfa6b
hippocanfly <hippomayfly@gamil.com>
2025-04-12 11:19:11 +0800

    feat(ListNode): 添加链表的 toString 方法和静态打印方法
    - 在 ListNode 类中添加了重写的 toString 方法，可以将链表转换为字符串表示
    - 添加了静态的 toString 方法，用于打印链表- 这些方法有助于在调试和测试时更方便地查看链表的内容

--- src/main/java/com/so/common/ListNode.java
+++ src/main/java/com/so/common/ListNode.java
@@ -13,4 +13,36 @@ public class ListNode {
         this.next = null;
         this.random = null;
     }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        ListNode current = this;
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) {
+                sb.append("->");
+            }
+            current = current.next;
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 静态方法：打印链表
+     */
+    public static String toString(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        ListNode current = head;
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) {
+                sb.append("->");
+            }
+            current = current.next;
+        }
+        return sb.toString();
+    }
+
 }

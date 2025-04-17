3b8977d202f2bf10d78db1327b4122dfc5a2e9de
HippoFly <hippomayfly@gmail.com>
2025-04-17 15:47:42 +0800

    refactor(leetcode35): 优化第 35 题搜索插入位置的代码
    - 添加了问题描述和示例输入输出，提高了代码的可读性和完整性
    - 优化了变量命名和格式化，提升了代码的可读性- 添加了对算法思路的注释，帮助理解二分查找的过程
    - 删除了多余的注释和打印语句，精简了代码

--- src/main/java/com/so/lc/leetcode/Q35_SearchInsertPosition.java
+++ src/main/java/com/so/lc/leetcode/Q35_SearchInsertPosition.java
@@ -1,41 +1,52 @@
 package com.so.lc.leetcode;
 
 /**
- * 描述
+ * 35. 搜索插入位置
  * #二分查找
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * <p>
+ * 请必须使用时间复杂度为 O(log n) 的算法。
+ *
+ * 示例 1:
+ * <p>
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
  *
  * @author FlyHippo
  * @version 1.0
  * @createDate 2024/5/3 22:31
+ * @tag 二分查找
  **/
 
 public class Q35_SearchInsertPosition {
 
     public int searchInsert(int[] nums, int target) {
         int len = nums.length;
-        int left=0,right= len -1;
+        int left = 0, right = len - 1;
 
 
-        // 特殊判断
+        // 特殊判断：仅仅做最大越界判断，是因为需要扩容后的一位，靠二分法没法得到这一位
+        // 二分仅仅能在数组内部找到目标值
         if (nums[len - 1] < target) {
             return len;
         }
-        // [1,3,5,6](5)为案例 left因num[1]<5而变2，然后mid=2,num[mid]得5没法小于target，
-        // 让right=mid, 这退出循环
-        // 最终目的是让left步进从而mid处等于target，同时左无法满足mid小于target
-        // right直接压过来等于mid
+        // while最后一定以两者相等退出循环
+        while (left < right) {
 
-        // [1,3,6,7](5)为案例 left因num[1]<5而变2，然后mid=2,num[mid]得6没法小于target=5，
-        // 让right=mid, 这退出循环
-        // 最终目的是让left步进从而mid处没法小于target，同时左无法满足mid小于target
-        // right直接压过来等于mid， 此时由于‘/’符号去尾号的效应刚好会等于目标插处
-        while (left<right){
-            int mid = (left+right)/2;
-            if(nums[mid]<target){
+
+            // 长度为偶数8，（0+7）/ 2 = 3 mid为第4个元素，
+            // 长度为偶数7，（0+6）/ 2 = 3 mid为第4个元素，
+            int mid = (left + right) / 2;
+
+            if (nums[mid] < target) {
                 left = mid + 1;
                 //我的疑惑在这，
-            }else  {
-                right = mid ;
+            } else {
+                right = mid;
             }
         }
         return right;
@@ -43,6 +54,6 @@ public class Q35_SearchInsertPosition {
 
     public static void main(String[] args) {
         Q35_SearchInsertPosition insertPosition = new Q35_SearchInsertPosition();
-        System.out.println(insertPosition.searchInsert(new int[]{1, 3,  6,7}, 5));
+        System.out.println(insertPosition.searchInsert(new int[]{1, 3, 6, 7}, 5));
     }
 }

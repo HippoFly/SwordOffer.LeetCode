86f07beaf436861fe3f62baf3f1d61dbd4e3d386
HippoFly <hippomayfly@gmail.com>
2025-05-05 11:28:25 +0800

    feat(Q287): 添加二分法解题思路并优化代码注释
    - 新增二分法解题的函数 findDuplicate2
    - 为现有解法添加详细注释，包括链表快慢指针法的原理说明- 更新类名中的方法描述，明确指出是第 287 题

--- src/main/java/com/so/lc/leetcode/Q287_FindDuplicateNumber.java
+++ src/main/java/com/so/lc/leetcode/Q287_FindDuplicateNumber.java
@@ -1,7 +1,7 @@
 package com.so.lc.leetcode;
 
 /**
- * 描述
+ * 287. 寻找重复数
  *
  * @author FlyHippo
  * @version 1.0
@@ -10,9 +10,13 @@ package com.so.lc.leetcode;
 
 public class Q287_FindDuplicateNumber {
     /**
+     * 链表快慢指针法（抽象）
+     * <p>
      * 初始化： 定义两个指针，一个慢指针 slow 和一个快指针 fast，初始时它们都指向数组的第一个元素。
+     * 原理
      * 移动： 快指针 fast 每次移动两步，慢指针 slow 每次移动一步，直到它们相遇。此时，快指针移动的总步数是慢指针的两倍。
      * 检测环： 如果快指针 fast 和慢指针 slow 相遇，说明数组中存在环。此时将快指针重新置为数组的第一个元素，并且将两个指针以相同的速度移动，直到它们再次相遇。相遇的点就是环的入口，即重复的数字。
+     *
      * @param nums
      * @return
      */
@@ -40,6 +44,30 @@ public class Q287_FindDuplicateNumber {
         return slow;
 
     }
+
+    /**
+     * 二分法（好记）
+     *
+     * @param nums
+     * @return
+     */
+    public int findDuplicate2(int[] nums) {
+        int left = 1, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) count++;
+            }
+            if (count > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
 
     public static void main(String[] args) {
         Q287_FindDuplicateNumber findDuplicateNumber = new Q287_FindDuplicateNumber();

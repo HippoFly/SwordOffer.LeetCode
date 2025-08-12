cbac5561a6ae9784b616545a39dc6b049a7baf1d
HippoFly <hippomayfly@gmail.com>
2025-08-12 15:48:23 +0800

    feat(Q53_MaxSubarray): 添加贪心算法解决方案
    - 实现了贪心算法来解决最大子数组问题
    - 添加了详细的代码注释，解释了贪心

--- src/main/java/com/so/lc/leetcode/Q53_MaxSubarray.java
+++ src/main/java/com/so/lc/leetcode/Q53_MaxSubarray.java
@@ -26,6 +26,47 @@ package com.so.lc.leetcode;
  * @link <a href="https://leetcode.cn/problems/maximum-subarray/">最大子数组</a>
  *
  **/
-// TODO
 public class Q53_MaxSubarray {
+
+    /**
+     * 贪心算法
+     *贪心思路的核心直觉
+     * 我们遍历数组时，其实在做一个选择：
+     * 当前子数组要不要继续加下去？还是重新开始一个新的子数组？
+     *
+     * 直觉：
+     * 如果当前子数组的和已经是负数，那它只会拖累后面的结果，立刻丢掉，重新开始。
+     * 如果是正数，那它会让后面更大，继续加。
+     *
+     *
+     *
+     * 解题：
+     * currentSum = 当前子数组的和
+     * maxSum = 遍历到目前为止的最大子数组和
+     *
+     * 流程：
+     * 从左到右扫描
+     * 每遇到新元素 x，判断：
+     * 如果 currentSum < 0，丢掉它，从 x 重新开始
+     * 否则加上 x
+     * 每一步更新 maxSum
+     *
+     *
+     * @param nums
+     * @return
+     */
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];  // 全局最大和
+        int currentSum = nums[0];  // 当前子数组和
+
+        for (int i = 1; i < nums.length; i++) {
+            // 要么继续之前的子数组，要么从当前元素重新开始（这里巧妙在 max（）如果 cS 是负数会导致其加上 num[i]后变小）
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            // 更新全局最大值 （仅仅在当前连续和挑战成功会更替）
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
 }

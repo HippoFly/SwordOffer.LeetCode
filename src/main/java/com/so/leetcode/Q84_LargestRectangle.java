630156a17511f34a0ec92dbb5abaf206b7e23320
HippoFly <hippomayfly@gmail.com>
2025-09-07 14:43:27 +0800

    refactor(Q84): 优化柱状图最大矩形面积计算逻辑
    - 移除了创建新数组的步骤，直接在原数组基础上进行计算
    - 添加了注释来解释代码逻辑和关键步骤
    - 优化了变量命名，使代码更易理解和维护
    - 简化了循环条件，通过在末尾添加一个虚拟柱子（高度为0）来强制触发栈的弹出操作

--- src/main/java/com/so/leetcode/Q84_LargestRectangle.java
+++ src/main/java/com/so/leetcode/Q84_LargestRectangle.java
@@ -16,46 +16,33 @@ public class Q84_LargestRectangle {
     /**
      * 计算柱状图中最大的矩形面积
      * 单调栈
+     * •
+     * 1.每个柱子都入栈，但如果没有发生单调变化，我们放内部不管
+     * 2.只有当前柱子比栈顶柱子矮时：说明其已经产生变小的威胁，弹出并计算它，但是不用担心其之后哪怕更矮会通过宽度弥补，因为它本身以后还要入栈
      *
      * @param heights 柱状图的高度数组
      * @return 最大矩形面积
      */
     public int largestRectangleArea(int[] heights) {
-        // 在 heights 数组两端添加高度为 0 的柱子，以便处理边界情况
-        int[] newHeights = new int[heights.length + 2];
-        newHeights[0] = 0;
-        newHeights[newHeights.length - 1] = 0;
-        // 使用 for 循环复制 heights 数组到 newHeights 数组
-        for (int i = 0; i < heights.length; i++) {
-            newHeights[i + 1] = heights[i];
-        }
-
-        int n = newHeights.length;
+        int n = heights.length;
         Stack<Integer> stack = new Stack<>();
         int maxArea = 0;
-        /*
-        对每个柱子，做两件事：
 
-        如果当前柱子比栈顶柱子高，就直接把索引压进栈，保持递增。
-        如果当前柱子矮于栈顶柱子，说明栈顶柱子再往右延伸就不行了，所以需要：
-        弹出栈顶元素，计算以这个弹出的柱子为“最矮高度”的矩形面积。
-        矩形的宽度是：i - stack.peek() - 1
-        解释：i是当前柱子的位置，stack.peek()是弹出后的栈顶（也就是左边第一个比它矮的柱子）。
-        中间夹着的那些柱子都比弹出的那个高，所以宽度是两者之间的数量。
-        然后不断更新最大面积。
-        */
-        // 遍历新的高度数组
-        for (int i = 0; i < n; i++) {
-            // 当栈不为空且当前柱子高度小于栈顶柱子高度时，计算矩形面积
-            while (!stack.isEmpty() && newHeights[i] < newHeights[stack.peek()]) {
-                int height = newHeights[stack.pop()];
-                // -1是当前差距的数量
-                int width = i - stack.peek() - 1;
+        // 遍历每个柱子
+        for (int i = 0; i <= n; i++) {
+            // 当前高度（这里特意让 i 可以==n，超出数组范围时设为0）
+            /*
+            * 强制触发栈的弹出操作 当 i = n时，currHeight = 0（虚拟柱子），此时所有栈中剩余的柱子都会满足 currHeight < heights[stack.peek()]，从而被弹出并计算面积。
+            * */
+            int currHeight = (i == n) ? 0 : heights[i];
 
+            // 当遇到比栈顶柱子矮的柱子时，代表其对比，要开始计算面积
+            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()]; // 弹出栈顶柱子的高度
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
                 maxArea = Math.max(maxArea, height * width);
             }
-            // 当前柱子的索引入栈
-            stack.push(i);
+            stack.push(i); // 当前索引入栈
         }
 
         return maxArea;

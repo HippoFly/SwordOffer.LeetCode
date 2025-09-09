73a540a347dec2cbcb3a4493f26d8f17bdd57f5c
HippoFly <hippomayfly@gmail.com>
2025-09-09 16:42:26 +0800

    feat(Q136_SingleNumber): 添加异或解法并优化代码
    - 新增异或解法的 singleNumber 方法，时间复杂度为 O(n)
    - 保留原有的暴力解法，并命名为 singleNumber2
    - 添加代码注释，解释异或解法的原理和步骤
    - 优化代码格式和命名，提高可读性

--- src/main/java/com/so/leetcode/Q136_SingleNumber.java
+++ src/main/java/com/so/leetcode/Q136_SingleNumber.java
@@ -8,20 +8,45 @@ import java.util.Set;
  * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
  */
 public class Q136_SingleNumber {
+
+    /**
+     * 异或解法
+     * 1.
+     * 任何数与 0 异或等于它本身：a ^ 0 = a
+     * 2.
+     * 任何数与自身异或等于 0：a ^ a = 0
+     * 3.
+     * 异或满足交换律和结合律：a ^ b ^ a = (a ^ a) ^ b = 0 ^ b = b
+     * @param nums
+     * @return
+     */
     public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // 对所有数字进行异或操作
+        }
+        return result;
+    }
+
+    /**
+     *   暴力解法
+     * @param nums
+     * @return
+     */
+    public int singleNumber2(int[] nums) {
 
         Set<Integer> temp = new HashSet<>();
         for (int num : nums) {
-            if(temp.contains(num)){
+            if (temp.contains(num)) {
                 temp.remove(num);
-            }else {
+            } else {
                 temp.add(num);
             }
         }
-            if(temp.size()>0){
-                Integer[] integers = temp.toArray(new Integer[0]);
-                return integers[0];
-            }
+        if (temp.size() > 0) {
+            Integer[] integers = temp.toArray(new Integer[0]);
+            return integers[0];
+        }
         return -1;
     }
 }

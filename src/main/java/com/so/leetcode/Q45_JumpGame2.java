f1a94b34e0104272102471c6f33db163400b0d51
HippoFly <hippomayfly@gmail.com>
2025-09-09 16:26:39 +0800

    refactor(leetcode): 优化跳跃游戏 II 代码
    - 添加测试用例和注释，提高代码可读性
    - 优化变量命名和代码结构，增强代码逻辑性
    - 更新问题描述，使其更加清晰准确

--- src/main/java/com/so/leetcode/Q45_JumpGame2.java
+++ src/main/java/com/so/leetcode/Q45_JumpGame2.java
@@ -1,8 +1,10 @@
 package com.so.leetcode;
 
+import org.junit.Test;
+
 /**
  *  跳跃游戏 II
- * 【不再输出能否，而是最小步数】
+ * 【不再输出能否，而是最小步数：跳跃到最后索引的最小步数】
  *  给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
  *
  * 每个元素 nums[i] 表示从索引 i 向后跳转的最大长度。换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
@@ -19,17 +21,22 @@ package com.so.leetcode;
  * @link <a href=""></a>
  **/
 public class Q45_JumpGame2 {
+    /**
+     * 不停更新能到最远位置farthest
+     * 但是仅有到边界end才会跳和运用不停更新能到最远位置farthest 这个不停更新的边界 减少跳跃次数
+     * @param nums
+     * @return
+     */
     public int jump(int[] nums) {
         int jumps = 0;       // 跳跃次数
         int farthest = 0;    // 当前能到达的最远位置
-        int end = 0;         // 当前跳跃步的边界
+        int end = 0;         // 当前跳跃步的边界，表示这一步能覆盖的范围。
 
         for (int i = 0; i < nums.length - 1; i++) {
-            // 更新当前位置能跳到的最远索引
+            // 更新当前位置i + 当前能跳距离=  能跳到的最远索引
             farthest = Math.max(farthest, i + nums[i]);
 
-            // 如果到了当前步能跳跃的 边界
-            if (i == end) {
+            if (i == end) { //如果 i == end，说明已经走到了当前跳跃步的边界，必须进行下一次跳跃：
                 //更新步数
                 jumps++;
                 //更新边界
@@ -38,5 +45,10 @@ public class Q45_JumpGame2 {
         }
 
         return jumps;
+    }
+
+    public static void main(String[] args) {
+        Q45_JumpGame2 jumpGame2 = new Q45_JumpGame2();
+        System.out.println(jumpGame2.jump(new int[]{2,3,1,1,4})); //先跳一步到 3，再跳一步到 4
     }
 }
